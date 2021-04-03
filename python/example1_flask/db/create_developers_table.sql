63a9be399d62abc253901a174c7268099617ac03
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-04-02 23:05:59 -0500

    Updated example1_flask with db sql creation

---
+++ python/example1_flask/db/create_developers_table.sql
@@ -0,0 +1,31 @@
+-- SIMPLE DATABASE CONFIGURATION FOR APPLICATION
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET @@global.time_zone = "+00:00";
+--
+-- Database: `employees`
+--
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+--
+-- Structure for the database: `developers`
+--
+CREATE TABLE `developers` (
+    `id` int  NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `lastname` varchar(255) NOT NULL,
+    `area` varchar(255) NOT NULL,
+    `age` int NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+--
+-- Add some sample data for the data of table: `developers`
+--
+INSERT INTO `developers` (`name`, `lastname`, `area`, `age`)
+VALUES ('Santiago', 'Garcia', 'DevOps', 21),
+    ('Monica', 'Hill', 'Data Science', 22),
+    ('Elkin', 'Guerra', 'Robotics', 30),
+    ('Melissa', 'Mejia', 'Web', 40);
+COMMIT;
