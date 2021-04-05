acde668aec8c6749fd4e81dabff9b8c2b370e137
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-04-04 23:51:46 -0500

    Updated python example1_flask deleted unnecessary templates

--- python/example1_flask/tests/test_paths.py
+++ python/example1_flask/tests/test_paths.py
@@ -1,6 +1,29 @@
 import requests
 
 BASE_URL = "http://localhost:5000/"
+body = {
+    "name": "Camila",
+    "lastname": "Yepes Ruiz",
+    "id_type": "cc",
+    "id_value": "4444",
+    "area": "Infrastructure",
+    "age": 22
+}
 
-response = requests.get(BASE_URL + "developers/1")
+response = requests.get(BASE_URL + "developers/cc/0000")
 print(response.json())
+print(response.status_code)
+
+response = requests.put(BASE_URL + "developers/cc/4444", body)
+print(response.json())
+print(response.status_code)
+
+response = requests.get(BASE_URL + "developers/cc/4444")
+print(response.json())
+print(response.status_code)
+
+response = requests.delete(BASE_URL + "developers/cc/4444")
+print(response)
+
+response = requests.delete(BASE_URL + "developers/cc/4444")
+print(response)
