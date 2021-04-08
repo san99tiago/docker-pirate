9e37e49cbe178f72625ad26725ab7b00bf2f4ec6
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-04-08 12:28:40 -0500

    Updated express-mysql with env variable for sonarqube host

--- express-mysql/app/sonar-project.js
+++ express-mysql/app/sonar-project.js
@@ -1,14 +1,15 @@
 const sonarqubeScanner = require("sonarqube-scanner");
 sonarqubeScanner(
   {
-    serverUrl: "http://localhost:9000",
+    serverUrl: process.env.SONARQUBE_HOST || "http://localhost:9000",
     // token: "",
     options: {
       "sonar.projectName": "SuperSimpleExpressExample",
       "sonar.projectVersion": "0.0.1",
       "sonar.projectDescription": "A simple app to apply a SonarQube",
       "sonar.projectKey": "SuperSimpleExpressExample:Test",
-      "sonar.sources": "./app.js, ./config/db.config.js, ./routes/developers.js",
+      "sonar.sources":
+        "./app.js, ./config/db.config.js, ./routes/developers.js",
       "sonar.tests": "./test",
       "sonar.javascript.lcov.reportPaths": "./coverage/lcov.info",
       // "sonar.inclusions": "./", // Entry point of your code
