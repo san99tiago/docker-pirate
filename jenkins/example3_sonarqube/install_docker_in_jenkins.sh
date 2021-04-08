ab149bc2db472ed60b8b9ef9a13c3b52929da4f7
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-04-08 16:09:57 -0500

    Updated express-mysql with jenkins-sonarqube docker-compose upgrades and shell instructions

---
+++ jenkins/example3_sonarqube/install_docker_in_jenkins.sh
@@ -0,0 +1,13 @@
+apt-get update
+
+apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg > /tmp/dkey;apt-key add /tmp/dkey
+
+add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs)  stable"
+
+apt-get update
+
+apt-get -y install docker-ce
+
+usermod -a -G docker jenkins
