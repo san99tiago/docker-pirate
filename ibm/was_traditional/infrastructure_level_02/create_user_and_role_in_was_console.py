173f58acdba8640f725ecc5901d1d106f1b73ffd
Santiago Garcia Arango <santiago.garcia1999@hotmail.com>
2022-03-07 15:10:53 -0500

    Update create_user_and_role script for IBM WAS

--- ibm/was_traditional/infrastructure_level_02/create_user_and_role_in_was_console.py
+++ ibm/was_traditional/infrastructure_level_02/create_user_and_role_in_was_console.py
@@ -1,14 +1,14 @@
 # WSADMIN SCRIPT TO CREATE A USER WITH SPECIFIC ROLE FOR IBM WAS CONSOLE
 # Santiago Garcia Arango
 # Command:
-# wsadmin.sh -lang jython -f /tmp/test.py "was_monitor" "1234" "monitor" "WAS" "MONITOR"
+# wsadmin.sh -lang jython -f /tmp/monitor.py "was_monitor" "1234" "monitor" "WAS" "MONITOR"
 
 import sys
 import os
 import re
 
 if(len(sys.argv) < 5):
-    print("\n***** This script will be skipped because it expects more arguments *****")
+    print("\n***** This script " + "configure_was_monitor.py" + " will be skipped because it expects more arguments *****")
     os._exit()
 
 user_id = sys.argv[0]  # Unique ID for the user that will be created [was_monitor]
@@ -20,7 +20,7 @@ user_last_name = sys.argv[4]  # Specifies the last name or family name of the us
 
 # Create the user that will be mapped to its "monitor" configuration below
 try:
-    print("\n***** Creating/validating user {} *****".format(user_id))
+    print("\n***** Creating/validating user %s *****" % (user_id))
     print(AdminTask.createUser('[-uid ' + user_id + ' -password ' + user_password + ' -confirmPassword ' + user_password + ' -cn ' + user_first_name + ' -sn ' + user_last_name + ']'))
     AdminConfig.save()
 except:
@@ -31,10 +31,10 @@ current_auth_users = AdminTask.listUserIDsOfAuthorizationGroup()
 print(current_auth_users)
 role_already_attached = re.findall(user_role + "=\[" + user_id + "\]", current_auth_users)
 if(len(role_already_attached) > 0):
-    print("\n***** The user {} already has role {} *****".format(user_id, user_role))
+    print("\n***** The user %s already has role %s *****" % (user_id, user_role))
 else:
     # Map the created user to the "monitor" role in the 0=defaultWIMFileBasedRealm
-    print("\n***** Mapping user {} to role {} *****".format(user_id, user_role))
+    print("\n***** Mapping user %s to role %s *****" % (user_id, user_role))
     AdminTask.mapUsersToAdminRole('[-accessids [user:defaultWIMFileBasedRealm/uid=' + user_id + ',o=defaultWIMFileBasedRealm ] -userids [' + user_id + ' ] -roleName ' + user_role + ']')
     AdminConfig.save()
 
