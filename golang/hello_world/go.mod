afc9d2c233126b3845745c7b54a756d9c31c102f
Santiago Garcia Arango <santiago.garcia1999@hotmail.com>
2022-04-12 09:50:37 -0500

    Add initial golang hello_world template

---
+++ golang/hello_world/go.mod
@@ -0,0 +1,3 @@
+module example.com/hello_world
+
+go 1.17
---
+++ golang/hello_world/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for {
+		fmt.Println("Hello my amazing friends, I am Santi!!!")
+		time.Sleep(time.Second * 3)
+	}
+}
