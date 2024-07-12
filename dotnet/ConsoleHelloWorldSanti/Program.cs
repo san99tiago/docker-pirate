1f2603d014aab24c60fb5b4a446543f7ba8e26f4
Santiago Garcia Arango <santiago.garcia1999@hotmail.com>
2024-07-12 00:28:09 -0500

    Add demo dotnet application

---
+++ dotnet/ConsoleHelloWorldSanti/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1) Hello by Santi!");
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("2) This is my hello-world app with C# + .NET!");
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("3) I am not a Windows fan, but just to keep learning!");
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("4) Goodbye by Santi!");
